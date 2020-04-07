753c0a64ad83fb684a049a51341a2d10773b7b61
jerrymanalil <62981166+jerrymanalil@users.noreply.github.com>
2020-04-07 19:36:54 +0530

    test

---
+++ EmployeeWebAPIService/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeWebAPIService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+
+        public IEnumerable<Employee> Get(int pageIndex = 1)
+        {
+            using (AngularDemoEntities entities = new AngularDemoEntities())
+            {
+                return entities.Employees.Take(pageIndex * 2).ToList();
+            }
+        }
+
+        public Employee Get(string code)
+        {
+            int currentPageIndex = 1;
+            int.TryParse(code, out currentPageIndex);
+            using (AngularDemoEntities entities = new AngularDemoEntities())
+            {
+                return entities.Employees.FirstOrDefault(emp => emp.Code.Equals(code));
+            }
+        }
+    }
+}
